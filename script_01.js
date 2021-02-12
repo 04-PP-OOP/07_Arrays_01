c5130f72609b5ae91366f4c781c544d28114767b
Peter <schmidt.multimedia@gmail.com>
2021-02-12 10:02:44 +0100

    01 / satzbau 01 - Einzelparameter

--- script_01.js
+++ script_01.js
@@ -2,9 +2,18 @@
 /*** 1.0 Arrays */
 
 
-
-
-
+/*** Funktionalität mit Einzelparametern */
+ausgabe(getSentence("Ich","bin","Drazen","Savi")); // Test
+function getSentence(word1,word2,word3,word4) {
+    let gap = " ";
+    let punct = ".";
+    let str =   word1 + gap + 
+                word2 + gap + 
+                word3 + gap + 
+                word4 + 
+                punct;
+    return str;  
+}
 
 // Modul: Ausgabe in Konsole | Test:
 //ausgabe("hi");
