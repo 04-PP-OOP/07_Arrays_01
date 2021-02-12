d1645906b7fdaa3a7036d9d845ed864202109570
Peter <schmidt.multimedia@gmail.com>
2021-02-12 10:29:45 +0100

    02 / Array - basics

--- script_01.js
+++ script_01.js
@@ -1,9 +1,24 @@
 
 /*** 1.0 Arrays */
 
+/*
+let arr;
+arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,6,7,12];
+arr = [true,false];
+arr = ["Ich","bin","Drazen"];
+
+ausgabe(arr);
+ausgabe(arr[0]);
+ausgabe(arr[2]);
+ausgabe(arr.length);
+*/
+
+
 
 /*** Funktionalität mit Einzelparametern */
-ausgabe(getSentence("Ich","bin","Drazen","Savi")); // Test
+//ausgabe(getSentence("Ich","bin","Drazen","Savi")); // Test
 function getSentence(word1,word2,word3,word4) {
     let gap = " ";
     let punct = ".";
